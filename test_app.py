457ba4b1d51d63dad75a5a62b7083008b9cbd962
Ruel Arguelles <arguellesruel03@gmail.com>
2025-03-02 20:10:44 +0800

    Update test_app.py
    xcc

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import add
 
 class TestApp(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 7)
+        self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
 
